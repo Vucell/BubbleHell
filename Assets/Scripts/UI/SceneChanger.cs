f939dc228bcbf0a9a51bcf26045eefdbd3477d7a
SkyCrasher9 <9000206@alumnos.ufv.es>
2025-01-25 21:12:14 +0100

    Cambios en el menu y creditos

--- Assets/Scripts/UI/SceneChanger.cs
+++ Assets/Scripts/UI/SceneChanger.cs
@@ -6,5 +6,21 @@ public class SceneChanger : MonoBehaviour
     {
         EditorSceneManager.LoadScene("SampleScene");
     }
-    
+
+    public void RestartGame()
+    {
+        EditorSceneManager.LoadScene("SampleScene");
+    }
+
+    public void BackToMain()
+    {
+        EditorSceneManager.LoadScene("Menu");
+        Debug.Log("To the main menu");
+    }
+
+    public void ToCredits()
+    {
+        EditorSceneManager.LoadScene("Credits");
+        Debug.Log("To the credits");
+    }
 }
